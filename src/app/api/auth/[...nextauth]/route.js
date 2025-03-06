a41d8d8b32c77e3f5c2e8201d6bba45ec0aea2ff
rakategar <tegarrakaw@gmail.com>
2025-03-06 09:12:24 +0700

    new file:   package-lock.json 	modified:   package.json 	new file:   src/app/api/auth/[...nextauth]/route.js 	new file:   src/app/api/register/route.js 	new file:   src/app/api/userExists/route.js 	modified:   src/app/layout.js 	new file:   src/app/login/page.jsx 	modified:   src/app/page.js 	new file:   src/app/register/page.jsx

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,68 @@
+import { connectMongoDB } from "../../../../../lib/mongodb";
+import User from "../../../../../models/user";
+import NextAuth, { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {},
+      async authorize(credentials) {
+        const { email, password } = credentials;
+
+        try {
+          await connectMongoDB();
+          const user = await User.findOne({ email });
+
+          if (!user) {
+            return null;
+          }
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (!passwordsMatch) {
+            return null;
+          }
+
+          return {
+            id: user._id,
+            email: user.email,
+            username: user.username,
+          };
+        } catch (error) {
+          console.log("Error: ", error);
+          return null;
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.username = user.username;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.username = token.username;
+      return session;
+    },
+  },
+};
+
+export const GET = async (req, res) => {
+  return NextAuth(req, res, authOptions);
+};
+
+export const POST = async (req, res) => {
+  return NextAuth(req, res, authOptions);
+};
