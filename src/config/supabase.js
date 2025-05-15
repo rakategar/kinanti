dd1035366741467dfe1ef8f6498a0a1e21c3d89b
Raka Tegar W <118181347+rakategar@users.noreply.github.com>
2025-05-15 10:15:04 +0700

    sangat baru

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,7 +1,7 @@
 const { createClient } = require("@supabase/supabase-js");
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_KEY;
+const SUPABASE_URL = "https://wgdxgzraacfhfbxvxuzy.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndnZHhnenJhYWNmaGZieHZ4dXp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwMzYzOTgsImV4cCI6MjA1NjYxMjM5OH0.E81IkESQjwL8KLYYaiyYcGgZNiANWef3szZxtPusJz8";
 
 // 🔥 Validasi biar ga kosong
 if (!SUPABASE_URL || !SUPABASE_KEY) {
